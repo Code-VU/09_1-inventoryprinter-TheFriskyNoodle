265f0e4cd3cf44d5f392528e936fbd8c3dee7171
Tom Brentlinger <Tom.Brentlinger@vu.com>
2023-04-02 14:14:41 -0500

    Teaser

--- displayInventory.py
+++ displayInventory.py
@@ -2,7 +2,8 @@ stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 
 def displayInventory(inventory):
     # your code goes here
-
+    for a,b in stuff.items():
+        print(a, b)
 
 if __name__ == "__main__":
     displayInventory(stuff)
