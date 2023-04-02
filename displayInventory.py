84ec7bc149d7cfa5aa9f53d7e9605b2f993a1702
Tom Brentlinger <Tom.Brentlinger@vu.com>
2023-04-02 14:24:15 -0500

    Fixes

--- displayInventory.py
+++ displayInventory.py
@@ -1,9 +1,11 @@
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
-
+total = None
 def displayInventory(inventory):
     # your code goes here
+    print('Inventory: ')
     for a,b in stuff.items():
         print(a, b)
-
+        total = b + b
+    print('Total number of items:', total)
 if __name__ == "__main__":
     displayInventory(stuff)
